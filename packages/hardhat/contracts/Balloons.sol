07021c020a230c836e8f9bb8893213e5f5c68d3f
mugrebot <89887146+mugrebot@users.noreply.github.com>
2021-11-27 15:19:14 -0500

    First commit
    yeet

--- packages/hardhat/contracts/Balloons.sol
+++ packages/hardhat/contracts/Balloons.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Balloons is ERC20 {
   constructor() ERC20("Balloons", "BAL") {
-      // **You can update the msg.sender address with your 
+      // **You can update the msg.sender address with your
       // front-end address to mint yourself tokens.
       _mint(0x697D940BC9A2aa8F56c1eC65E640951781C98A23, 1000 ether);
       // This mints to the deployer
--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -26,7 +26,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "rinkeby";
 
 const mainnetGwei = 21;
 
@@ -34,7 +34,7 @@ function mnemonic() {
   try {
     return fs.readFileSync("./mnemonic.txt").toString().trim();
   } catch (e) {
-    if (defaultNetwork !== "localhost") {
+    if (defaultNetwork !== "rinkeby") {
       console.log(
         "☢️ WARNING: No mnemonic file created for a deploy account. Try `yarn run generate` and then `yarn run account`."
       );
@@ -66,10 +66,10 @@ module.exports = {
   networks: {
     localhost: {
       url: "http://localhost:8545",
-      /*      
+      /*
         notice no mnemonic here? it will just use account 0 of the hardhat node to deploy
         (you can put in a mnemonic here to set the deployer locally)
-      
+
       */
     },
 
@@ -100,7 +100,7 @@ module.exports = {
     // },
 
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/460f40a260564ac4a4f4b3fffb032dad", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://rinkeby.infura.io/v3/53984c0a6b4248039e4968fec4076893", // <---- YOUR INFURA ID! (or it won't work)
 
       //    url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXX/eth/rinkeby", // <---- YOUR MORALIS ID! (not limited to infura)
 
